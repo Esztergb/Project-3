14d896cc7af56f768b07d36a9a24746dce9d3a38
Eszter Barbuscia <gubiscom@yahoo.com>
2023-09-13 11:11:19 -0700

    proxy setting

--- server/server.js
+++ server/server.js
@@ -2,11 +2,21 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 // const routes = require('./routes'); // fixed if routes folder isnt used
+
+// const routes = require('./routes'); // fixed if routes folder isnt used
 const spoonacularRoute = require('./utils/API');
+const { ApolloServer } = require('@apollo/server');
+const { typeDefs , resolvers } = require('./schemas');
+const { authMiddlewear } = require('./utils/auth');
+const { expressMiddleware } = require('@apollo/server/express4');
 
 const app = express();
+const server = new ApolloServer ({
+  typeDefs,
+  resolvers,
+  context: authMiddlewear,
+});
 const PORT = process.env.PORT || 3001;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api/spoonacular', spoonacularRoute);
@@ -15,9 +25,24 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
+
+app.get( '*' , (req ,res) => {
+  res.sendFile(path.join(__dirname , '../client/dist/index.html'))
+})
+
+const startApolloServer = async () => {
+  await server.start();
+  app.use(express.urlencoded({ extended: true }));
+  app.use(espress.json());
+  app.use('/graphql' , expressMiddleware(server));
+  db.once('open' , () => {
+    app.listen(PORT , () => {
+      console.log(`API server running on Port ${PORT} 🚀`);
+    })
+  })
+};
+
 // app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
 
+startApolloServer();
