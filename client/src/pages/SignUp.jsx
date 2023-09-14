81f430ecf999f5883470d65a1ccb6bc18ba15555
LenZuro <126126939+LenZuro@users.noreply.github.com>
2023-09-13 22:04:35 -0700

    login and signup ready for backend

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,52 +1,106 @@
-import React from 'react'
+import { useState } from "react";
 
-export default function SignUp() {
+const SignUp = () => {
+  const [userFormData, setUserFormData] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  // const [login] = useMutation(LOGIN_USER);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handelFormSubmit = async (event) => {
+    event.preventDefault();
+
+    console.log(userFormData);
+  };
+
   return (
-    <div className="w-full h-screen flex bg-cwhite md:flex md:justify-center mb-6">
-    <div className="mt-[80px]">
-    <form className="w-full max-w-sm">
-  <div className="md:flex md:items-center mb-6">
-    <div className="md:w-1/3">
-      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-        Full Name
-      </label>
-    </div>
-    <div className="md:w-2/3">
-      <input className="bg-cbrown appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-cbrown" id="inline-full-name" type="text" placeholder='Jane Doe'></input>
-    </div>
-  </div>
-  <div className="md:flex md:items-center mb-6">
-    <div className="md:w-1/3">
-      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
-        Password
-      </label>
-    </div>
-    <div className="md:w-2/3">
-      <input className="bg-cbrown appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-cbrown" id="inline-password" type="password" placeholder="******************"></input>
-    </div>
-  </div>
-  <div className="md:flex md:items-center mb-6">
-    <div className="md:w-1/3"></div>
-    <label className="md:w-2/3 block text-gray-500 font-bold">
-      <input className="mr-2 leading-tight" type="checkbox"></input>
-      <span className="text-sm">
-        Send me your newsletter!
-      </span>
-    </label>
-    </div>
-  <div className="md:flex md:items-center">
-    <div className="md:w-1/3"></div>
-    <div className="md:w-2/3">
-      <button className="text-cwhite border bg-cbrown border-cbrown
-    hover:bg-transparent hover:text-cbrown rounded-md px-8 py-3" type="button">
-        Sign Up
-      </button>
-    </div>
-  </div>
-</form>
-    </div>
-  </div>
-    
-    
+    <form className="w-full max-w-sm" onSubmit={handelFormSubmit}>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label
+            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            htmlFor="username"
+          >
+            Username
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            name="username"
+            type="text"
+            value={userFormData.username}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label
+            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            htmlFor="email"
+          >
+            Email
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            name="email"
+            type="email"
+            value={userFormData.email}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label
+            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            htmlFor="password"
+          >
+            Password
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            name="password"
+            type="password"
+            value={userFormData.password}
+            placeholder="******************"
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3"></div>
+        <label className="md:w-2/3 block text-gray-500 font-bold">
+          <input className="mr-2 leading-tight" type="checkbox" />
+          <span className="text-sm">Send me your newsletter!</span>
+        </label>
+      </div>
+      <div className="md:flex md:items-center">
+        <div className="md:w-1/3"></div>
+        <div className="md:w-2/3">
+          <button
+            className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </form>
   );
-}
+};
+
+export default SignUp;
