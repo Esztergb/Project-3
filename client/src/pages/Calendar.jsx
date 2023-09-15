52ea37542c160dfe51cd984b13d5edd5f8630faf
Eszter Barbuscia <gubiscom@yahoo.com>
2023-09-14 20:09:48 -0700

    weeklymenu"

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -2,8 +2,51 @@ import React from 'react'
 
 function Calendar() {
   return (
-    <div>Calendar</div>
-  )
+    <div className="mx-[5rem] my-[5rem]">
+      <div className="flex">
+        {/* Left Column (1/3 of the screen) */}
+        <div className="w-1/3 bg-gray-200 p-4">
+          <div className="mb-4">
+            <button className="bg-cgreen hover:bg-blue-600 text-white font-bold h-12 w-full rounded">
+              Monday
+            </button>
+          </div>
+          <div className="mb-4">
+            <button className="bg-cgreen hover:bg-blue-600 text-white font-bold h-12 w-full rounded">
+              Tuesday
+            </button>
+          </div>
+          <div className="mb-4">
+            <button className="bg-cgreen hover:bg-blue-600 text-white font-bold h-12 w-full rounded">
+              Wednesday
+            </button>
+          </div>
+          <div className="mb-4">
+            <button className="bg-cgreen hover:bg-blue-600 text-white font-bold h-12 w-full rounded">
+              Thursday
+            </button>
+          </div>
+          <div className="mb-4">
+            <button className="bg-cgreen hover.bg-blue-600 text-white font-bold h-12 w-full rounded">
+              Friday
+            </button>
+          </div>
+          <div className="mb-4">
+            <button className="bg-cgreen hover:bg-blue-600 text-white font-bold h-12 w-full rounded">
+              Saturday
+            </button>
+          </div>
+          <div>
+            <button className="bg- hover:bg-blue-600 text-white font-bold h-12 w-full rounded">
+              Sunday
+            </button>
+          </div>
+        </div>
+        {/* Right Column (2/3 of the screen) */}
+        <div className="w-2/3 bg-white p-4">{/* Menu Cards Go Here */}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Calendar
