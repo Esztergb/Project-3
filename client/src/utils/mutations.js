e35eee1addfd4f64f492e4a371384bb62cd84de4
itsa-me-dea <wachadea@gmail.com>
2023-09-16 12:16:11 -0700

    fixed typo on mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,26 +1,25 @@
-import { gpl } from "@apollo/client";
+import { gql } from "@apollo/client";
 
-export const LOGIN_USER = gpl`
-mutation login($email: String!, $password: String!) {
-    login(email: $email , password: $password) {
-        token
-        user {
-            _id
-            username
-        }
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
-}
+  }
 `;
 
-export const ADD_USER = gpl`
-mutation adduser($username: String! , $email: String! , $password: String!) {
-    addUser(username: $username , email: $email , password: $password){
-        token
-        user{
-            _id
-            username
-        }
+export const ADD_USER = gql`
+  mutation adduser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
-}
+  }
 `;
-
